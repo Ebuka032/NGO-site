958b3b6476369099f8f6c815f04045b4450a881f
Stking19 <okolistephen43@gmail.com>
2025-02-25 10:35:29 -0800

    working on home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,18 @@
+import "../Style/home.css";
 
 const Home = () => {
   return (
-    <div>Home</div>
-  )
-}
+    <>
+      <div className="imgk">
+        <p>
+          MAKE A CHILD SMILE <br /> TODAY, LITTLE THINGS <br />
+          MATTERS.
+        </p>
+        <button>DONATE NOW</button>
+      </div>
+      <div className="impactk"></div>
+    </>
+  );
+};
 
-export default Home
+export default Home;
