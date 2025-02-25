f6f0bd4e85afef65a71e1ec683ad3daf3963d9ba
olukosiadebayo <adebayolukmon@gmail.com>
2025-02-26 00:55:36 +0100

    added a paragraph

---
+++ src/pages/Contact.jsx
@@ -0,0 +1,69 @@
+import React from 'react'
+import './contact.css'
+
+const Contact = () => {
+
+  return (
+    <>
+     <div className='header-contact'>
+        <h2>Contact Us</h2>
+        <p>You can call in or send an email.</p>
+     </div>
+
+     <div className='content'>
+        <div className='image-section'>
+            <img src="" alt="Group" />
+            <img src="" alt="group-image" />
+        </div>
+
+        <div className='form-section'>
+            <h2>CONTACT US</h2>
+            <h3>Get in Touch with us</h3>
+
+            <form>
+              <div className="form-row">
+               <input type="text" placeholder="First Name" />
+               <input type="text" placeholder="Last Name" />
+              </div>
+              <input type="email" placeholder="Email" />
+              <input type="text" placeholder="Subject" />
+              <textarea placeholder="Subject"></textarea>
+              <button type="submit">SEND REQUEST</button>
+            </form>
+           </div>
+          </div>
+
+          <div>
+          <div className="info-section">
+        <div className="info-column">
+          <h2>Volunteering</h2>
+          <p>For further information on how you can volunteer,<br /> 
+          please call: +234(0)809 608 3336, +234(0)809 527 1645, +234(0)908 784 9642</p>
+        </div>
+        <div className="info-column">
+          <h2>Partnership</h2>
+          <p>For further information on how you partner with us, please call: +234(0)809 608 3336, +234(0)809 527 1645,<br />
+           +234(0)908 784 9642</p>
+        </div>
+      </div>
+      <div className="map-section">
+        <p>Emmanuel Cathedral</p>
+        <a href="view-larger-map-link">View larger map</a>
+        <img src="path/to/map-image.png" alt="Map" className="map-image" /> 
+      </div>
+    </div>
+
+
+
+           
+       
+
+        
+       
+
+
+    </>
+  )
+}
+
+export default Contact
