86a88b438ba6c63ebeaf543e9fbfe9823e796c4f
PopeFrancis003 <onyekwereobinna1@gmail.com>
2025-02-25 22:46:40 +0100

    changd font size

--- src/components/static/Header.jsx
+++ src/components/static/Header.jsx
@@ -9,15 +9,15 @@ const Header = () => {
                 <img src="/public/logo.png" alt="" />
             </div>
             <div className="text">
-                <h4>Home</h4>
-                <h4>About Us</h4>
-                <h4>Features Page</h4>
-                <h4>Get Involve</h4>
-                <h4>Contact Us</h4>
+                <h4>HOME</h4>
+                <h4>ABOUT US</h4>
+                <h4>FEATURES PAGE</h4>
+                <h4>GET INVOLVE</h4>
+                <h4>CONTACT US</h4>
             </div>
             <div className="right">
                 <div className="donate">
-                    <p>Donate</p>
+                    <p>DONATE</p>
                 </div>
             </div>
         </div>
