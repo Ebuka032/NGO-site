e17058ac828ce11cac86589a946289edca9b4f21
Perpetual <zeeoneperppy@gmail.com>
2025-02-26 17:55:56 +0100

    updated Code

--- src/pages/Donate.jsx
+++ src/pages/Donate.jsx
@@ -1,24 +1,47 @@
 import React from 'react'
+import './Donate.css'
 
 const Donate = () => {
   return (
     <div className='donateBody'>
-      <div className='donateDiv'>
-       <div className='donateHead'></div> 
-
-
-
-
-
-
-
-
-
-
-
-
+      <div className='donateHeader'>
 
       </div>
+
+      <div className='donateMoney'>
+        <h1>MONEY DONATE</h1>
+      </div>
+
+      <div className='donateDesc'>
+        <div className='descRiption'>
+
+            <p>
+             CHFI with your generous support, recently donated food items to over 50 households , provided scholarship for 2 students,
+             <br />
+             educational materials to over 10 students, donated over 600 books to 2 schools, provided medical support in 2 communities and
+             <br />
+             donated 50 packs of reuseable microbial sanitary pads.
+             <br />
+             <br />
+             To support CFHI to distribute relief materials to children in  orphanages and impoverished communities across Nigeria,please
+             <br />
+             call<h5>08095271645</h5> or send an email to <h5>info@cfhinitiative.org</h5> and make your contirbutions to;
+            </p>
+
+            <div className='descImage'>
+                <img src="" alt="" />
+            </div>
+          
+          <div className='donatePay'>
+            <div></div>
+            <div></div>
+            <div></div>
+          </div>
+
+
+        </div>
+      </div>
+
     </div>
   )
 }
