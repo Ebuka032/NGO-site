b5ff0a32758d4ea0db882a952ec416bd0fffd354
PopeFrancis003 <onyekwereobinna1@gmail.com>
2025-02-26 22:14:29 +0100

    footer updated

--- src/components/static/Footer.jsx
+++ src/components/static/Footer.jsx
@@ -1,8 +1,38 @@
 import React from 'react'
-
+import "../../Style/Footer.css"
+import { RiFacebookCircleFill } from "react-icons/ri";
+import { AiFillTwitterCircle } from "react-icons/ai";
+import { AiFillInstagram } from "react-icons/ai";
+import { IoMailSharp } from "react-icons/io5";
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div className='footer_body'>
+        <div>
+            <div className='logo'>
+                <img  src="/public/logo.png" alt=""/>
+            </div>
+        </div>
+        <div className='text'>
+            <h3>Our contacts<br/><br/>
+            8, Exellence & Friends street<br/>
+            Dawaki,Abuja (FCT) Nigeria<br/><br/>
+            Exellence House, plot 5001<br/>
+            Commercial Action Area, New<br/>
+            Owerri, Imo State Nigeria.</h3>
+            <h3>Mobile:<br/>
+            +234 809 608 3359,<br/>
+            +234 809 608 3336<br/><br/>
+            FOLLOW US:<br/>
+            <RiFacebookCircleFill />
+            <AiFillTwitterCircle/>
+            <AiFillInstagram/>
+            <IoMailSharp/></h3>
+            
+
+            <h4></h4>
+
+        </div>
+    </div>
   )
 }
 
