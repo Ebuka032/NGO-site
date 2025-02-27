26bb3307fc2334057bc5d9248180193b85175acf
Agu Chukwuebuka Ezekiel <aguchukwuebuka03@gmail.com>
2025-02-27 14:00:26 -0800

    Update

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
 import "./Header.css"
 import { NavLink } from 'react-router'
+import logo from "../../assets/Images/logo.png"
+
 
 const Header = () => {
   return (
     <div className='header_body'>
         <div className="header_wrapper">
             <div className="logo">
-                <img src="/logo.png" alt="" />
+                <img src={logo} alt="" />
             </div>
             <div className="text">
                 <NavLink to="/" className="nav-link">
